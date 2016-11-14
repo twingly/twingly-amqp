d768ffe94277e8f796270deba752faac07e6ba32
Robin Wallin <robin@twingly.com>
2016-11-14 11:47:55 +0100

    Satisfy RuboCop

--- lib/twingly/amqp/null_logger.rb
+++ lib/twingly/amqp/null_logger.rb
@@ -2,9 +2,13 @@ module Twingly
   module AMQP
     class NullLogger
       def debug(*); end
+
       def info(*); end
+
       def warn(*); end
+
       def error(*); end
+
       def fatal(*); end
     end
   end
