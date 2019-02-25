10e9d9674479d38976738321a0ba5ce974249644
Mattias Roback <mattias.roback@twingly.com>
2019-02-25 17:16:09 +0100

    Publisher: Extract all duplicated methods to a base class
    Was trying to fix #71, and noticed we had to do the same thing in
    two places, so I extracted the methods to a base class instead.
    
    I don't remember if there was the reason to why we have two similar
    classes though.

--- lib/twingly/amqp/topic_exchange_publisher.rb
+++ lib/twingly/amqp/topic_exchange_publisher.rb
@@ -1,47 +1,14 @@
-require "twingly/amqp/connection"
-require "json"
-require "ostruct"
+require "twingly/amqp/base_publisher"
 
 module Twingly
   module AMQP
-    class TopicExchangePublisher
+    class TopicExchangePublisher < BasePublisher
       def initialize(exchange_name:, routing_key: nil, connection: nil, opts: {})
+        super(connection: connection)
+
         options.routing_key = routing_key
 
-        connection ||= Connection.instance
-        @exchange = connection.create_channel.topic(exchange_name, opts)
-      end
-
-      def publish(message)
-        raise ArgumentError unless message.respond_to?(:to_h)
-
-        payload = message.to_h.to_json
-        opts    = options.to_h
-        @exchange.publish(payload, opts)
-      end
-
-      # only used by tests to avoid sleeping
-      def publish_with_confirm(message)
-        channel = @exchange.channel
-        channel.confirm_select unless channel.using_publisher_confirmations?
-
-        publish(message)
-
-        @exchange.wait_for_confirms
-      end
-
-      def configure_publish_options
-        yield options
-      end
-
-      private
-
-      def options
-        @options ||=
-          OpenStruct.new(
-            content_type: "application/json",
-            persistent: true,
-          )
+        @exchange = @connection.create_channel.topic(exchange_name, opts)
       end
     end
   end
