883cb0f3668782a6f0bb6058ce45408d92f089d0
Mattias Roback <mattias.roback@twingly.com>
2021-01-28 17:01:41 +0100

    Close channel after queue has been created
    The point of this method is just to create a queue, not to return
    anything, so it's fine to just close the channel after.
    
    See https://github.com/twingly/twingly-amqp/pull/93#discussion_r565374046

--- lib/twingly/amqp/utilities.rb
+++ lib/twingly/amqp/utilities.rb
@@ -2,13 +2,13 @@ module Twingly
   module AMQP
     module Utilities
       def self.create_queue(queue_name, durable: true, arguments: {}, connection: Connection.instance)
-        channel = connection.create_channel
-
-        channel.queue(
-          queue_name,
-          durable: durable,
-          arguments: arguments,
-        )
+        connection.with_channel do |channel|
+          channel.queue(
+            queue_name,
+            durable: durable,
+            arguments: arguments,
+          )
+        end
       end
     end
   end
