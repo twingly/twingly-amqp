085e21d9d43f15ef7b32a9c51c5b3a4dd71d5034
Mattias Roback <mattias.roback@twingly.com>
2021-02-01 09:26:56 +0100

    Utilties spec improvements
    * Add describe block for "Utilties.create_queue" method
      https://github.com/twingly/twingly-amqp/pull/93#discussion_r566814563
    * No need for long describe for spec, the default one is enough
      https://github.com/twingly/twingly-amqp/pull/93#discussion_r566812763
    * Add better describe for the "optional queue arguments" hash for ".create_queue",
      the default one wasn't good. RabbitMQ themselves calls it "optional queue arguments"
      at https://www.rabbitmq.com/queues.html#optional-arguments
      https://github.com/twingly/twingly-amqp/pull/93#discussion_r566822922

--- spec/integration/twingly/amqp/utilities_spec.rb
+++ spec/integration/twingly/amqp/utilities_spec.rb
@@ -1,61 +1,64 @@
 describe Twingly::AMQP::Utilities do
-  let(:queue_name)      { "twingly-amqp.utilities.test.queue" }
-  let(:amqp_connection) { Twingly::AMQP::Connection.instance }
+  describe ".create_queue" do
+    let(:queue_name)      { "twingly-amqp.utilities.test.queue" }
+    let(:amqp_connection) { Twingly::AMQP::Connection.instance }
 
-  subject do
-    described_class.create_queue(queue_name, connection: amqp_connection)
-  end
-
-  after do
-    amqp_connection.with_channel do |channel|
-      channel.queue_delete(queue_name)
-    end
-  end
-
-  it "creates the queue" do
-    expect{ subject }
-      .to change { amqp_connection.queue_exists?(queue_name) }
-      .from(false)
-      .to(true)
-  end
-
-  it "returns a Bunny::Queue to be able to verify options in the specs" do
-    expect(subject).to be_a(Bunny::Queue)
-  end
-
-  describe "queue options" do
-    subject(:created_queue) do
-      described_class.create_queue(queue_name, connection: amqp_connection, **options)
+    subject do
+      described_class.create_queue(queue_name, connection: amqp_connection)
     end
 
-    let(:options) { {} }
-
-    describe "durable:" do
-      subject { created_queue.durable? }
-
-      context "by default" do
-        it { is_expected.to be true }
-      end
-
-      context "with durable: false" do
-        before { options[:durable] = false }
-
-        it { is_expected.to be false }
+    after do
+      amqp_connection.with_channel do |channel|
+        channel.queue_delete(queue_name)
       end
     end
 
-    describe "arguments:" do
-      subject { created_queue.arguments }
+    it "creates the queue" do
+      expect{ subject }
+        .to change { amqp_connection.queue_exists?(queue_name) }
+        .from(false)
+        .to(true)
+    end
 
-      context "by default" do
-        it { is_expected.to be_empty }
+    it { is_expected.to be_a(Bunny::Queue) }
+
+    describe "queue options" do
+      subject(:created_queue) do
+        described_class.create_queue(queue_name, connection: amqp_connection, **options)
       end
 
-      context "when given some queue arguments" do
-        let(:arguments_hash) { { "x-some-argument": "some-value" } }
-        before { options[:arguments] = arguments_hash }
+      let(:options) { {} }
 
-        it { is_expected.to eq(arguments_hash) }
+      describe "durable:" do
+        subject { created_queue.durable? }
+
+        context "by default" do
+          it { is_expected.to be true }
+        end
+
+        context "with durable: false" do
+          before { options[:durable] = false }
+
+          it { is_expected.to be false }
+        end
+      end
+
+      describe "arguments:" do
+        subject { created_queue.arguments }
+
+        context "by default" do
+          it { is_expected.to be_empty }
+        end
+
+        context "with a hash containing optional queue arguments" do
+          let(:arguments_hash) { { "x-some-argument": "some-value" } }
+
+          before { options[:arguments] = arguments_hash }
+
+          it "creates queue with the given arguments" do
+            is_expected.to eq(arguments_hash)
+          end
+        end
       end
     end
   end
